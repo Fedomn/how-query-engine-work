5edd673ffe4239ac46fbd0d94229c22126e8b029
Fedomn <fedomn.ma@gmail.com>
2021-12-04 14:39:55 +0800

    refactor parquet data source to support iteration

--- datasource/parquet_data_source.go
+++ datasource/parquet_data_source.go
@@ -16,11 +16,22 @@ type ParquetDataSource struct {
 	schema    datatypes.Schema
 	batchSize int
 	pr        *reader.ParquetReader
+	// current scan row pos
+	cursor int64
+	// the number of rows in recordBatch
+	numRows int64
+
+	// projection schema
+	pjSchema datatypes.Schema
+	// projection indices
+	pjIndices []int
+	// arrow array builders
+	builders []datatypes.ArrowArrayBuilder
 }
 
-func NewParquetDataSource(filename string, batchSize int) *ParquetDataSource {
+func NewParquetDataSource(filename string, batchSize int, projection []string) *ParquetDataSource {
 	p := &ParquetDataSource{filename: filename, batchSize: batchSize}
-	p.inferSchema()
+	p.inferSchema(projection)
 	return p
 }
 
@@ -28,39 +39,32 @@ func (p *ParquetDataSource) Schema() datatypes.Schema {
 	return p.schema
 }
 
-func (p *ParquetDataSource) Scan(projection []string) []datatypes.RecordBatch {
-	readSchema, readIndices := p.schema.SelectByName(projection)
-	res := make([]datatypes.RecordBatch, 0)
-	builders := make([]datatypes.ArrowArrayBuilder, 0)
-	for _, field := range readSchema.Fields {
-		builders = append(builders, datatypes.NewArrowArrayBuilder(memory.NewGoAllocator(), field.DataType))
+func (p *ParquetDataSource) Scan() datatypes.RecordBatch {
+	for i, pjIdx := range p.pjIndices {
+		data, _, _, err := p.pr.ReadColumnByIndex(int64(pjIdx), int64(p.batchSize))
+		if err != nil {
+			panic(fmt.Sprintf("parquet read data err: %v", err))
+		}
+		p.builders[i].AppendValues(data...)
 	}
 
-	readCnt := 0
-	rowCnt := p.pr.GetNumRows()
-	for readCnt < int(rowCnt) {
-		for i := 0; i < len(readIndices); i++ {
-			data, _, _, err := p.pr.ReadColumnByIndex(int64(readIndices[i]), int64(p.batchSize))
-			if err != nil {
-				panic(fmt.Sprintf("parquet read data err: %v", err))
-			}
-			for _, datum := range data {
-				builders[i].Append(datum)
-			}
-		}
-
-		fields := make([]datatypes.ColumnArray, len(readSchema.Fields))
-		for i := 0; i < len(builders); i++ {
-			fields[i] = builders[i].Build()
-		}
-		res = append(res, datatypes.RecordBatch{Schema: readSchema, Fields: fields})
-		readCnt += p.batchSize
+	p.cursor += int64(p.batchSize)
+	fields := make([]datatypes.ColumnArray, len(p.pjIndices))
+	for i := 0; i < len(p.builders); i++ {
+		fields[i] = p.builders[i].Build()
 	}
-	return res
+	return datatypes.RecordBatch{
+		Schema: p.schema,
+		Fields: fields,
+	}
+}
+
+func (p *ParquetDataSource) Next() bool {
+	return p.cursor < p.numRows
 }
 
 // parquet read refer to https://github.com/xitongsys/parquet-go/blob/master/tool/parquet-tools/parquet-tools.go
-func (p *ParquetDataSource) inferSchema() {
+func (p *ParquetDataSource) inferSchema(projection []string) {
 	fr, err := local.NewLocalFileReader(p.filename)
 	if err != nil {
 		panic(fmt.Sprintf("Can't open file: %v", err))
@@ -78,6 +82,14 @@ func (p *ParquetDataSource) inferSchema() {
 
 	p.schema = datatypes.Schema{Fields: headers}
 	p.pr = pr
+	p.cursor = 0
+	p.numRows = p.pr.GetNumRows()
+
+	p.pjSchema, p.pjIndices = p.schema.SelectByName(projection)
+	p.builders = make([]datatypes.ArrowArrayBuilder, len(p.pjSchema.Fields))
+	for i, field := range p.pjSchema.Fields {
+		p.builders[i] = datatypes.NewArrowArrayBuilder(memory.NewGoAllocator(), field.DataType)
+	}
 }
 
 func (p *ParquetDataSource) createFiled(elem *parquet.SchemaElement) datatypes.Field {
--- datasource/parquet_data_source_test.go
+++ datasource/parquet_data_source_test.go
@@ -9,7 +9,7 @@ import (
 const filename = dir + "/alltypes_plain.parquet"
 
 func TestParquetDataSource_Schema(t *testing.T) {
-	pds := NewParquetDataSource(filename, 10)
+	pds := NewParquetDataSource(filename, 10, []string{})
 	headers := []string{"Id", "Bool_col", "Tinyint_col", "Smallint_col", "Int_col", "Bigint_col", "Float_col", "Double_col", "Date_string_col", "String_col", "Timestamp_col"}
 	for i, header := range headers {
 		require.Equal(t, header, pds.Schema().Fields[i].Name)
@@ -17,24 +17,43 @@ func TestParquetDataSource_Schema(t *testing.T) {
 }
 
 func TestParquetDataSource_Scan_with_no_projection(t *testing.T) {
-	pds := NewParquetDataSource(filename, 5)
-	result := pds.Scan([]string{})
-	recordBatch := result[0]
-
+	pds := NewParquetDataSource(filename, 1204, []string{})
+	recordBatch := pds.Scan()
 	firstColumn := recordBatch.Field(0)
-	firstColumnData := []int32{4, 5, 6, 7, 2}
+	firstColumnData := []int32{4, 5, 6, 7, 2, 3, 0, 1}
 	for i, datum := range firstColumnData {
 		require.Equal(t, datum, firstColumn.GetValue(i))
 	}
 }
 
 func TestParquetDataSource_Scan_with_projection(t *testing.T) {
-	pds := NewParquetDataSource(filename, 2)
-	result := pds.Scan([]string{"Timestamp_col"})
-	recordBatch := result[0]
-
+	pds := NewParquetDataSource(filename, 1024, []string{"Timestamp_col"})
+	recordBatch := pds.Scan()
 	firstColumn := recordBatch.Field(0)
 	firstValue := firstColumn.GetValue(1)
 	time := types.INT96ToTime(firstValue.(string))
 	require.Equal(t, "2009-03-01 00:01:00 +0000 UTC", time.String())
+}
+
+func TestParquetDataSource_Scan_with_smallBatch(t *testing.T) {
+	pds := NewParquetDataSource(filename, 3, []string{"Bool_col"})
+
+	require.True(t, pds.Next())
+	res := pds.Scan()
+	require.Equal(t, 3, res.RowCount())
+
+	require.True(t, pds.Next())
+	res = pds.Scan()
+	require.Equal(t, 3, res.RowCount())
+
+	require.True(t, pds.Next())
+	res = pds.Scan()
+	require.Equal(t, 2, res.RowCount())
+	require.False(t, pds.Next())
+
+	firstColumn := res.Field(0)
+	require.Equal(t, 2, firstColumn.Size())
+
+	firstValue := firstColumn.GetValue(0)
+	require.Equal(t, true, firstValue)
 }
